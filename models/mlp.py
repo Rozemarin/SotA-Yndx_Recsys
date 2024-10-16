fe7cff787be9b7d13f4a0db5afa8c9332daeabdb
AnnaNik334743 <annanikiforova564@gmail.com>
2024-10-16 11:24:06 +0300

    Updated MLP

--- models/mlp.py
+++ models/mlp.py
@@ -2,18 +2,15 @@ import torch
 import torch.nn as nn
 
 class MLP(nn.Module):
-    def __init__(self, input_dim, hidden_dim=64, output_size=1, dropout=0.2):
+    def __init__(self, input_dim, hidden_dim=64, output_size=1, dropout=0.5):
         super(MLP, self).__init__()
         self.fc1 = nn.Linear(input_dim, hidden_dim)
-        self.fc2 = nn.Linear(hidden_dim, hidden_dim // 2)
-        self.fc3 = nn.Linear(hidden_dim // 2, output_size)
+        self.fc2 = nn.Linear(hidden_dim, output_size)
         self.dropout = nn.Dropout(dropout)
 
     def forward(self, user_emb, item_emb):
         x = torch.cat([user_emb, item_emb], dim=-1)
         x = torch.relu(self.fc1(x))
         x = self.dropout(x)
-        x = torch.relu(self.fc2(x))
-        x = self.dropout(x)
-        x = self.fc3(x)
-        return x
+        x = self.fc2(x)
+        return torch.sigmoid(x)
