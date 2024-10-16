eaf438c6816fc35bb6c48ceff78d6e29c09270ca
AnnaNik334743 <annanikiforova564@gmail.com>
2024-10-16 15:22:32 +0300

    Moved mol to models

--- models/mlp.py
+++ models/mlp.py
@@ -2,16 +2,22 @@ import torch
 import torch.nn as nn
 
 
-class MLP(nn.Module):
-    def __init__(self, input_dim, hidden_dim=64, output_size=1, dropout=0.5):
-        super(MLP, self).__init__()
-        self.fc1 = nn.Linear(input_dim, hidden_dim)
-        self.fc2 = nn.Linear(hidden_dim, output_size)
-        self.dropout = nn.Dropout(dropout)
+class MLP(torch.nn.Module):
+
+    def __init__(self, input_dim, embed_dims=[128], dropout=0.2, output_layer=True):
+        super().__init__()
+        layers = list()
+        for embed_dim in embed_dims:
+            layers.append(torch.nn.Linear(input_dim, embed_dim))
+            layers.append(torch.nn.BatchNorm1d(embed_dim))
+            layers.append(torch.nn.ReLU())
+            layers.append(torch.nn.Dropout(p=dropout))
+            input_dim = embed_dim
+        if output_layer:
+            layers.append(torch.nn.Linear(input_dim, 1))
+        self.mlp = torch.nn.Sequential(*layers)
 
     def forward(self, user_emb, item_emb):
         x = torch.cat([user_emb, item_emb], dim=-1)
-        x = torch.relu(self.fc1(x))
-        x = self.dropout(x)
-        x = self.fc2(x)
-        return x
+        return self.mlp(x)
+    
