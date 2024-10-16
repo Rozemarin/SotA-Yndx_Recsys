72bb462bae402d03cccf76fb4740bd0bacfba081
Rozemarin <45430474+Rozemarin@users.noreply.github.com>
2024-10-17 00:20:10 +0300

    fixed imports for SASRec

--- models/__init__.py
+++ models/__init__.py
@@ -2,3 +2,5 @@ from models.dot_product import *
 from models.fm import *
 from models.mlp import *
 from models.neumf import *
+from models.SASRec.SASRec_model import *
+from models.SASRec.SASRec_utils import *
